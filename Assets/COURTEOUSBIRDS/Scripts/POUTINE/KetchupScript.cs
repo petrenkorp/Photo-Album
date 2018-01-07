8692e4e99e5466c6efb7565aab4ce0970f255d5e
petrenkorp <edward.petrenko@gmail.com>
2018-01-06 20:59:40 -0500

    ketchup hit splat

--- Assets/COURTEOUSBIRDS/Scripts/POUTINE/KetchupScript.cs
+++ Assets/COURTEOUSBIRDS/Scripts/POUTINE/KetchupScript.cs
@@ -13,32 +13,58 @@ public class KetchupScript : MonoBehaviour {
 	public int delay = 2;
 	private SpriteRenderer selfSprite;
 	private Collider2D bounds;
+	private bool hasCollided = false;
 
 	// Use this for initialization
 	void Start () {
 		selfSprite = GetComponent<SpriteRenderer>();
 		bounds = GetComponent<Collider2D>();
+		Debug.Log(bounds.bounds.extents);
 		StartCoroutine(ketchupTimer());		
 	}
 
 	IEnumerator ketchupTimer() {
 		yield return new WaitForSecondsRealtime(delay);
-		selfSprite.sprite = ketchup2;
+		if (!hasCollided) {
+			selfSprite.sprite = ketchup2;
+			bounds.bounds.Expand(2);	//doesn't work - why?
+			Debug.Log(bounds.bounds.extents);
+		}
+		
 		yield return new WaitForSecondsRealtime(delay);
-		selfSprite.sprite = ketchup3;
+		if (!hasCollided) {
+			selfSprite.sprite = ketchup3;
+			bounds.bounds.Expand(2);
+			Debug.Log(bounds.bounds.extents);
+		}
 		yield return new WaitForSecondsRealtime(delay);
-		selfSprite.sprite = ketchup4;
+		if (!hasCollided) {
+			selfSprite.sprite = ketchup4;
+			bounds.bounds.Expand(2);
+			Debug.Log(bounds.bounds.extents);
+		}
 		yield return new WaitForSecondsRealtime(delay);
-		selfSprite.sprite = ketchup5;
+		if (!hasCollided) {
+			selfSprite.sprite = ketchup5;
+			bounds.bounds.Expand(2);
+			Debug.Log(bounds.bounds.extents);
+		}
+
 		yield return new WaitForSecondsRealtime(delay);
-		GetComponent<Rigidbody2D>().isKinematic = false;
+		if (!hasCollided) {
+			GetComponent<Rigidbody2D>().isKinematic = false;
+		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D other) {
+		selfSprite.sprite = ketchupSplat;
 		if (other.collider.tag == "Damager") {	//the bird
-
+			Debug.Log("hithithit");
+			//selfSprite.sprite = ketchupSplat;
+			//GetComponent<Rigidbody2D>().isKinematic = false;
+			hasCollided = true;
 		} else if (other.collider.tag == "Poutine") {	//poutine
-			selfSprite.sprite = ketchupSplat;
+			//selfSprite.sprite = ketchupSplat;
 		}
 	}
 }
